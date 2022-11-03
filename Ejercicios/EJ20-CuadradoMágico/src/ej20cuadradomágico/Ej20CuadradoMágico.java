8ba75a71fd6c05ec7cbd088e837f6321cf6c836e
Gonzalo Lucien Dominguez <gonzalolucien@gmail.com>
2022-11-03 16:51:30 -0300

    Tercer subida de archivos, ejercicios.

---
+++ Ejercicios/EJ20-CuadradoMágico/src/ej20cuadradomágico/Ej20CuadradoMágico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej20cuadradomágico;
+
+/**
+ *
+ * @author Motherfucker
+ */
+public class Ej20CuadradoMágico {
+
+    /**
+     * Un cuadrado mágico 3 x 3 es una matriz 3 x 3 formada por números del 1 al 9 donde la
+    suma de sus filas, sus columnas y sus diagonales son idénticas. Crear un programa que
+    permita introducir un cuadrado por teclado y determine si este cuadrado es mágico o no.
+    El programa deberá comprobar que los números introducidos son correctos, es decir,
+    están entre el 1 y el 9.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][] mMagica = new int[3][3];
+    }
+    
+}
